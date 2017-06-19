b88c3ac8e6fa1f411b6c877dd7b32f94c6ae01dc
InfinitePhil <philipm227@gmail.com>
2017-06-19 05:30:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ app.post("/link", function(req, res) {
       var body = req.body.item.message.message
       var name = req.body.item.message.from.name
       
-var cut = body.slice(4)
+var cut = body.slice(6)
 var sn = "https://umnprd.service-now.com/nav_to.do?uri=incident.do?sysparm_query=number="
 var send = sn.concat(cut)
 
 
-var link = "&lt;a href='" + send + "&gt; INC" + body + "&lt;/a&gt;"
+var link = "&lt;a href='" + send + "&gt;" + cut + "&lt;/a&gt;"
   
-  res.json({ color: 'green', message: `${link}`, notify: 'false', message_format: 'html'})
+  res.json({ color: 'green', message: `${link}`, notify: 'false'})
 })
 
 const port = Number(process.env.PORT || 7000)
