4076dc8fae03bcafa9da17452d1b3d0c4e9c5040
InfinitePhil <philipm227@gmail.com>
2017-06-19 04:24:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const bodyParser = require('body-parser')
 const express    = require('express')
-const winston    = require('winston')
+
 
 const app = express()
 app.use(bodyParser.json()) 
@@ -10,8 +10,7 @@ app.post("/link", (req, res) => {
   const message     = req.body.item.message.message
   const name        = req.body.item.message.from.name
  
-  winston.info(`Message Sent: ${message}`)
-  winston.info(`Sent By: ${name}`)
+
   
   res.json({ color: green, message: `https://umnprd.service-now.com/nav_to.do?uri=incident.do?sysparm_query=number= ${message}`})
 })
@@ -20,6 +19,6 @@ const port = Number(process.env.PORT || 7000)
 if(!module.parent) { 
   app.listen(port)
 }
-winston.info(`Server Started on port: ${port}`)
+
 
 module.exports = app
