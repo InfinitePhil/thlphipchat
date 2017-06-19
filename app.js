30e0e50b4714291f604cdc3a4ac229595d5015a2
InfinitePhil <philipm227@gmail.com>
2017-06-19 04:49:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const app = express()
 app.use(bodyParser.json()) 
 app.use(express.static('public'))
 
-app.post("/link", (req, res) => {
-  const message     = req.body.item.message.message
-  const name        = req.body.item.message.from.name
- 
+app.post("/link", function(req, res) {
 
+      var value = req.body.item.message.message
+      var name = req.body.item.message.from.name
+ 
 var sn = "https://umnprd.service-now.com/nav_to.do?uri=incident.do?sysparm_query=number="
 var send = sn.concat(message)
   
-  res.json({ color: 'green', message: `${name} entered the following text: ${send}`, notify: 'false', message_format: 'html'})
+  res.json({ color: 'green', message: `<a href=" ${send} ">Link</a>`, notify: 'false', message_format: 'html'})
 })
 
 const port = Number(process.env.PORT || 7000)
