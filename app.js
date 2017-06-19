3a9514cece7a4061abba3c1c67e58e07b7c3dce7
InfinitePhil <philipm227@gmail.com>
2017-06-19 06:16:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var send = sn.concat(cut)
 var link = "&lt;a href=&quot;" + send + "&quot;&gt;" + cut + "&lt;/a&gt;"
            
  
-  res.json({ color: 'green', message: `<a href=" ${send} ">-------------</a>`, notify: 'false'})
+  res.json({ color: 'green', message: `<a href="https://umnprd.service-now.com">-------------</a>`, notify: 'false'})
 })
 
 const port = Number(process.env.PORT || 7000)
