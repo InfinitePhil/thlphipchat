7ae0304f300e8a138a851c53348d453aa0a5d17c
InfinitePhil <philipm227@gmail.com>
2017-06-19 04:22:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser')
 const express    = require('express')
+const winston    = require('winston')
 
 const app = express()
 app.use(bodyParser.json()) 
@@ -9,13 +10,16 @@ app.post("/link", (req, res) => {
   const message     = req.body.item.message.message
   const name        = req.body.item.message.from.name
  
+  winston.info(`Message Sent: ${message}`)
+  winston.info(`Sent By: ${name}`)
   
-  res.json({ color: green, message: `https://umnprd.service-now.com/nav_to.do?uri=incident.do?sysparm_query=number=' + message'})
+  res.json({ color: green, message: `https://umnprd.service-now.com/nav_to.do?uri=incident.do?sysparm_query=number= ${message}`})
 })
 
 const port = Number(process.env.PORT || 7000)
 if(!module.parent) { 
   app.listen(port)
 }
+winston.info(`Server Started on port: ${port}`)
 
 module.exports = app
