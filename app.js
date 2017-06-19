63cc65f27f0416b9cbaf9520088f6454fcb43bc5
InfinitePhil <philipm227@gmail.com>
2017-06-19 04:18:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.post("/link", (req, res) => {
   const name        = req.body.item.message.from.name
  
   
-  res.json({ message: `https://umnprd.service-now.com/nav_to.do?uri=incident.do?sysparm_query=number=' ${message} })
+  res.json({ color: green, message: `https://umnprd.service-now.com/nav_to.do?uri=incident.do?sysparm_query=number=' + message'})
 })
 
 const port = Number(process.env.PORT || 7000)
