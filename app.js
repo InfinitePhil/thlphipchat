7bc664c174a388217d4e494a64b40f1098d81086
InfinitePhil <philipm227@gmail.com>
2017-06-27 02:18:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -20,30 +20,9 @@ app.post("/link", (req, res) => {
 
 
 
-	switch (type) {
-		case "I":
-			var link = inc
-			break
-		case "K":
-			var link = kb
-			break
-		case "C":
-			var link = con
-			break
-		case "?":
-			res.json({
-				message: "Simply type /link followed by a full INC, KB, or CON number (including it's descriptor i.e. ‘INC’) and INCBot will return a link to the page. Note that the bot cannot check whether it is a valid number, only that it is formatted correctly. Put together by Phil, 2017.",
-				color: 'yellow'
-			})
-
-		default:
-			res.json({
-				message: "Either that isn't an INC, KB, or CON - or Phil coded me wrong... :(",
-				color: 'red'
-			})
-
-	}
-
+	
+	
+	
 
 	res.json({
 		message: `<a href="https://umnprd.service-now.com/nav_to.do?uri=${link}${cut}"> Here is ${cut}, ${firstname} :)</a>`,
