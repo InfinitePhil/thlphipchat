17c834252e80e78b50da2deda36956230e49f69f
InfinitePhil <philipm227@gmail.com>
2017-06-27 14:50:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ var app = express();
 app.use(bodyParser.json()); 
 
 // Route that Hipchat enters
-app.post("/link", function(req, res) {
+app.post("/hipchatbot", function(req, res) {
 
   // Gets the message value from the Hipchat JSON webhook
   var messageText = req.body.item.message.message;
   
   // Message posted back to Hipchat
-  res.json({
-		message: `${messageText}`,
-		color: 'green'
-	});
+  res.json({ message: `${messageText}`,
+				color: 'green'   });
+
+});
 
 // Giving the app a port number to listen on - use 3000 by default 
 var port = Number(process.env.PORT || 3000);
