eda252c0522b2cb154fb85ade131ff0e3a5bc934
InfinitePhil <philipm227@gmail.com>
2017-06-27 14:45:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-var express = require('express')
-var bodyParser = require('body-parser')
- 
-var app = express()
- 
-// create application/json parser 
-var jsonParser = bodyParser.json()
- 
-// create application/x-www-form-urlencoded parser 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
- 
-// POST /link gets urlencoded bodies 
-app.post('/link', urlencodedParser, function (req, res) {
-  var messageText = req.body.message.message
-})
+// Dependencies
+var bodyParser = require('body-parser');
+var express = require('express');
+
+// Calling the Express module
+var app = express();
+
+// Using the JSON function of the body-parser module
+app.use(bodyParser.json()); 
+
+// Route that Hipchat enters
+app.post("/link", function(req, res) {
+
+  // Gets the message value from the Hipchat JSON webhook
+  var message = req.body.item.message.message.image;
+  
+  // Message posted back to Hipchat
+  res.json({ image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2013/01/24/12/v2-cute-cat-picture.jpg"   });
+
+});
+
+// Giving the app a port number to listen on - use 3000 by default 
+var port = Number(process.env.PORT || 3000);
+
+// Starts the app
+app.listen(port);
