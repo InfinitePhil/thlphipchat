53411c8be6d16a891c6fe3e1188afcbe0fbe4757
InfinitePhil <philipm227@gmail.com>
2017-06-27 15:22:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,8 +15,42 @@ app.post('/link', jsonParser, function (req, res) {
   var messageText = fullmessageText.split('/link ')[1];
   var cleanText = messageText.split(' ')[0];
   
+	var inc = "incident.do?sysparm_query=number=";
+	var kb = "%2Fkb_view.do%3Fsysparm_article%3D";
+	var con = "textsearch.do?sysparm_search=";
+	
+	var link;
+	var firstname = name.split(' ')[0];
+
+
+
+	switch (type) {
+		case "I":
+			var link = inc;
+			break;
+		case "K":
+			var link = kb;
+			break;
+		case "C":
+			var link = con;
+			break;
+		case "?":
+			res.json({
+				message: "Simply type /link followed by a full INC, KB, or CON number (including it's descriptor i.e. ‘INC’) and INCBot will return a link to the page. Note that the bot cannot check whether it is a valid number, only that it is formatted correctly. Put together by Phil, 2017. Version 2.1",
+				color: 'yellow'
+			});
+
+		default:
+			res.json({
+				message: "Either that isn't an INC, KB, or CON - or Phil coded me wrong :(",
+				color: 'red'
+			});
+
+	}	
+	
+	
   // Message posted back to Hipchat
-  res.json({ message: `${cleanText}`,
+  res.json({ message: `<a href="https://umnprd.service-now.com/nav_to.do?uri=${link}${cleanText}"> Here is ${cleanText}, ${firstname} :)</a>`,
 				color: 'green'   });
 
 });
