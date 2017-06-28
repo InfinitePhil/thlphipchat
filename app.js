78f9d90ec3c75261838c94634d65fabf9be2e8b0
InfinitePhil <philipm227@gmail.com>
2017-06-27 21:50:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,29 +5,36 @@ var express = require('express');
 // Calling the Express module
 var app = express();
 
-var jsonParser = bodyParser.json({
-	extended: false
-})
+// create application/json parser
+var jsonParser = bodyParser.json()
 
-// POST /login gets urlencoded bodies
+// POST /login gets JSON bodies
 app.post('/link', jsonParser, function (req, res) {
 
-	// Gets the message value from the Hipchat JSON webhook
+	// Gets the full message value from the Hipchat JSON webhook inlcuding the /link
 	var fullmessageText = req.body.item.message.message;
-	var name = req.body.item.message.from.name;
-	var messageText = fullmessageText.split('/link ')[1];
-	var cleanText = messageText.split(' ')[0];
-	var type = cleanText.charAt(0);
 
+		// Removes the message text before and inluding the /link
+		var messageText = fullmessageText.split('/link ')[1];
+
+		// Removes the message text after the INC number
+		var cleanText = messageText.split(' ')[0];
+
+		// Gets first character of requested link to later determine what type it is - INC/KB etc.
+		var type = cleanText.charAt(0);
+	
+	// Gets the full name of the message author from the Hipchat JSON webhook
+	var fullName = req.body.item.message.from.name;
+
+		// Removes the middle and last names from name
+		var firstName = fullName.split(' ')[0];
+
+	// Base URL's for each respective link. Var's to make it easy if they change
 	var inc = "incident.do?sysparm_query=number=";
 	var kb = "%2Fkb_view.do%3Fsysparm_article%3D";
 	var con = "textsearch.do?sysparm_search=";
 
-	var link;
-	var firstname = name.split(' ')[0];
-
-
-
+	//Switch based on type of link will send to the right res.json which will POST to Hipchat
 	switch (type) {
 		case "I":
 			var link = inc;
@@ -57,17 +64,13 @@ app.post('/link', jsonParser, function (req, res) {
 				message: "Simply type /link followed by a full INC, KB, or CON number (including it's descriptor i.e. ‘INC’) and INCBot will return a link to the page. Note that the bot cannot check whether it is a valid number, only that it is formatted correctly. Put together by Phil, 2017. Version 2.1",
 				color: 'yellow'
 			});
-			break;
 
 		default:
 			res.json({
 				message: "Either that isn't an INC, KB, or CON - or Phil coded me wrong :(",
 				color: 'red'
 			});
-			break;
-
 	}
-
 
 });
 
